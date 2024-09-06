54a962fa779eb82855782b1270fdca82504f79c5
Ryan Peek <ryan.a.peek@gmail.com>
2024-09-06 13:37:19 -0700

    initialize code and gitignore

---
+++ code/04_pull_rgb_metrics.R
@@ -0,0 +1,259 @@
+# get GCC and Greenness Metrics from TimeLapse imagery
+# Ryan Peek, 2024
+
+# Library -----------------------------------------------------------------
+
+library(tidyverse)
+library(purrr)
+library(glue)
+library(fs)
+library(terra)
+
+# might be used in future
+#library(hazer)
+#library(daymetr)
+
+# Get Photo Directory --------------------------------------------
+
+#cam_region <- "R2" # dfw region
+cam_site <- "SAHE1" # location
+
+# Full path to folder where photos are located
+# this function helps select the folder and ensures there are images in the folder to use
+select_dir <- function(){
+  print("Select any image file WITHIN the folder you want to use:")
+  dirname(file.choose(new = TRUE))
+}
+
+# select an image from inside the folder of interest
+photo_directory <- select_dir()
+photo_directory
+
+# Read Photo Metadata -----------------------------------------------------
+
+# create path to drive location:
+photo_date_dir <- basename(photo_directory)
+exif_path <- fs::path_dir(photo_directory)
+
+# read in the exif metadata (run via 02_extract_metadata)
+photo_exif <- read_csv(glue("{exif_path}/pheno_exif_{cam_site}_{photo_date_dir}.csv.gz"))
+
+# Get Mask ----------------------------------------------------------------
+
+# Create NEW mask type and number (if first of type, 01)
+mask_type <-"GR_01_01" # i.e, "DB_01_01"
+
+# read in mask
+pheno_mask <- terra::rast(glue("{exif_path}/ROI/{cam_site}_{mask_type}.tif"))
+
+## TEST PLOTS --------------------------------------------------------------
+
+# test a single photo
+img <- terra::rast(photo_exif$full_path[10])
+img <- terra::flip(img) # note if not flipped masks will extract incorrect portion
+
+# plot to make sure mask is in the appropriate place...if not, need to redraw
+plotRGB(img)
+plot(pheno_mask, col=c(NA, alpha("yellow",0.8)), legend=FALSE, axes=FALSE, add=TRUE)
+
+# Filter to 1 Photo Per Day -----------------------------------------------
+
+photo_exif_noon <- photo_exif |>
+  # this filters to only photos at noon
+  filter(hms("12:00:00")==hms::as_hms(datetime))
+
+## this filters to any photos during 12pm hour
+##filter(12==hour(floor_date(datetime, unit="hour")))
+
+# Functions to Extract Time Series of Metrics ------------------------------
+
+# reads a single photo and generates the information/metrics of interest
+ph_get_CCC <- function(path, pheno_mask){
+  # read in data and crop
+  img <- terra::rast(path) # path to image
+  img <- terra::flip(img) # for some reason image is flipped so mask will not work
+  imask <- pheno_mask # spatrast mask
+  img_masked <- terra::mask(img, imask, inverse=TRUE, maskvalues=0)
+
+  # merge and calc CCs
+  img_matrix <- na.omit(as.matrix(img_masked))
+  tbl <- as.data.frame(t(apply(img_matrix, 2, quantile, na.rm=TRUE, probs = c(0, 0.05, 0.10, 0.25, 0.5, 0.75, 0.90, 0.95, 1))))
+  rownames(tbl) <- c('r','g','b')
+  colnames(tbl) <- c('min','q5', 'q10','q25','q50','q75', 'q90','q95','max')
+  RGB <- colMeans(img_matrix)
+
+  t <- rowSums(img_matrix)
+  ccMat <- apply(img_matrix, 2, '/', t)
+  cc <- colMeans(ccMat, na.rm = TRUE)
+  cc <- cc/sum(cc)
+
+  tbl$cc <- cc
+  tbl$std <- apply(ccMat, 2, sd)
+  tbl$brightness <- mean(apply(img_matrix, 2, max))
+  tbl$darkness <- mean(apply(img_matrix, 2, min))
+  tbl$contrast <- tbl$brightness - tbl$darkness
+  tbl$RGB <- RGB
+  return(tbl)
+}
+
+# reads all the photos in the list and applies the above function
+ph_make_CCC_ts <- function(photolist, pheno_mask){
+  require(data.table)
+
+  n <- length(photolist)
+  CCCT <- matrix(NA, nrow = n, ncol = 33)
+  for (i in 1:n) {
+    print(paste0("Extracting CCs for ", i))
+    tbl <- ph_get_CCC(photolist[i], pheno_mask)
+    if (!is.null(tbl))
+      CCCT[i, ] <- c(tbl$RGB, tbl$cc, tbl$std, tbl$q5,
+                     tbl$q10, tbl$q25, tbl$q50, tbl$q75, tbl$q90,
+                     tbl$q95, tbl$brightness[1], tbl$darkness[1],
+                     tbl$contrast[1])
+  }
+
+  CCCT <- as.data.table(CCCT)
+  colnames(CCCT) <- c("red", "green", "blue", "rcc", "gcc",
+                      "bcc", "rcc.std", "gcc.std", "bcc.std", "rcc05", "gcc05",
+                      "bcc05", "rcc10", "gcc10", "bcc10", "rcc25", "gcc25",
+                      "bcc25", "rcc50", "gcc50", "bcc50", "rcc75", "gcc75",
+                      "bcc75", "rcc90", "gcc90", "bcc90", "rcc95", "gcc95",
+                      "bcc95", "brightness", "darkness", "contrast")
+  CCCT[, `:=`(grR, gcc/rcc)]
+  CCCT[, `:=`(rbR, rcc/bcc)]
+  CCCT[, `:=`(gbR, gcc/bcc)]
+  CCCT[, `:=`(GRVI, (gcc - rcc)/(gcc + rcc))] # green-red vegetation index
+  CCCT[, `:=`(exG, (2 * green - red - blue))] # excess green
+  CCCT[, `:=`(file_name, (fs::path_file(photolist)))]
+  return(CCCT)
+}
+
+## Apply Functions ---------------------------------------------------------
+
+# extract all # TAKES A WHILE!!!
+system.time(df <- ph_make_CCC_ts(photo_exif_noon$full_path, pheno_mask))
+beepr::beep()
+
+# add datetime back for easier stitching
+df2 <- df |>
+  left_join(photo_exif_noon, by=c("file_name")) |>
+  #select(pheno_name, full_path, file_name:ambient_temperature, red:exG)
+  select(pheno_name, full_path, file_name:ambient_temp_C, red:exG)
+
+# Save Out ----------------------------------------------------------------
+
+# write out zipped versions to save space
+write_csv(df2, file = glue("{exif_path}/pheno_metrics_{cam_site}_{mask_type}_{photo_date_dir}_midday.csv.gz"))
+
+## Read in and Combine Series  ----------------------------------------------------------------
+
+# read in series matching mask:
+(pheno_files <- fs::dir_ls(glue("{exif_path}"),regexp = glue("{mask_type}")))
+
+df_mids <- read_csv(file = pheno_files)
+
+# VISUALIZATION --------------------------------------------------------------
+
+# filter to just one value at noon:
+df_mid <- df_mids |>
+  filter(hms("12:00:00")==hms::as_hms(datetime))
+
+# plot greeness from 90th percentile and excess greeness (exG)
+  #geom_violin(data=df_mid , aes(x=datetime, y=gcc90, group=month(datetime), fill=month(datetime)), alpha=0.4) +
+
+# add bg image
+library(ggimage)
+
+# set the date to use for these plots (depends on photo set)
+range(df_mid$datetime)
+photo_date_location <- "2024-08-10 00:00:00"
+
+ggplot() +
+  geom_point(data=df_mid , aes(x=datetime, y=gcc90), color="green4", alpha=0.5) +
+  hrbrthemes::theme_ipsum_rc() +
+  geom_smooth(data=df_mid, aes(x=datetime, y=gcc90)) +
+  scale_fill_viridis_c(option = "D", direction = -1) +
+  scale_x_datetime(date_breaks = "2 months", date_labels = "%Y-%b") +
+  labs(title="Greenness Index (GCC90)",
+       subtitle= glue("A metric tracking growth using RGB values (Mask: {mask_type})"),
+       x="", caption="Data from RECONYX hourly camera") +
+  geom_image(
+    data = tibble(datetime = ymd_hms(glue("{photo_date_location}")), gcc90 = 180),
+    aes(x=datetime, y=gcc90, image = glue("{exif_path}/ROI/pheno_{cam_site}_masked_{mask_type}.png")), size=0.5)
+
+
+ggsave(glue("figs/gcc90_{cam_site}_{mask_type}_midday.png"), width = 10, height = 8, dpi = 300, bg = "white")
+
+# GRVI: look at green red veg index which tracks senescence
+ggplot() +
+  geom_point(data=df_mid , aes(x=datetime, y=GRVI), color="green4") + hrbrthemes::theme_ipsum_rc() +
+  geom_smooth(data=df_mid, aes(x=datetime, y=GRVI)) +
+  scale_x_datetime(date_breaks = "2 months", date_labels = "%Y-%b") +
+  labs(title="Green-Red Vegetation Index (GRVI)",
+       subtitle= glue("A metric tracking growth and senescence using RGB values (Mask: {mask_type})"),
+       x="", caption="Data from RECONYX hourly camera") +
+  geom_image(
+    data = tibble(datetime = ymd_hms(glue("{photo_date_location}")), GRVI = 0.08),
+    aes(x=datetime, y=GRVI, image = glue("{exif_path}/ROI/pheno_{cam_site}_masked_{mask_type}.png")), size=0.4)
+
+ggsave(glue("figs/grvi_{cam_site}_{mask_type}_midday.png"), width = 10, height = 8, dpi = 300, bg = "white")
+
+# exGREEN
+ggplot() +
+  geom_smooth(data=df_mid, aes(x=datetime, y=exG)) +
+  geom_point(data=df_mid, aes(x=datetime, y=exG), fill="aquamarine4", size=3, pch=21, alpha=.8) +
+  hrbrthemes::theme_ipsum_rc() +
+  scale_x_datetime(date_breaks = "2 months", date_labels = "%Y-%b") +
+  labs(title="Excess Greeness (exG)",
+       subtitle= glue("A metric tracking excess greeness using RGB values (Mask: {mask_type})"),
+       x="", caption="Data from RECONYX hourly camera") +
+  geom_image(
+    data = tibble(datetime = ymd_hms(glue("{photo_date_location}")), exG = 90),
+    aes(x=datetime, y=exG, image = glue("{exif_path}/ROI/pheno_{cam_site}_masked_{mask_type}.png")), size=0.4)
+
+ggsave(glue("figs/exG_{cam_site}_{mask_type}_midday.png"), width = 10, height = 8, dpi = 300, bg = "white")
+
+# TIMESERIES ANALYSIS ------------------------------------------------------------------
+
+library(timetk)
+
+## GRVI: Different time periods ------------------------------------------
+
+df_mid %>%
+  plot_time_series_boxplot(datetime, GRVI, .period = "3 day",  .interactive = FALSE, .title = glue("GRVI at {cam_site}: 3 day"))
+
+df_mid %>%
+  plot_time_series_boxplot(datetime, GRVI, .period = "1 week",  .interactive = FALSE, .title = glue("GRVI at {cam_site}: 1 week"))
+
+df_mid %>%
+  plot_time_series_boxplot(datetime, GRVI, .period = "14 day",  .interactive = FALSE, .title = glue("GRVI at {cam_site}: 14 days"))
+
+## GCC: Different time periods ------------------------------------------
+
+df_mid %>%
+  plot_time_series_boxplot(datetime, gcc90, .period = "3 day",  .interactive = FALSE, .title = glue("Gcc90 at {cam_site}: 3 days"))
+
+df_mid %>%
+  plot_time_series_boxplot(datetime, gcc90, .period = "1 week",  .interactive = FALSE, .title = glue("Gcc90 at {cam_site}: 1 week"))
+
+df_mid %>%
+  plot_time_series_boxplot(datetime, gcc90, .period = "1 month",  .interactive = FALSE, .title = glue("Gcc90 at {cam_site}: 1 month"))
+
+
+# plot ACF diagnostics
+df_mid %>% plot_acf_diagnostics(datetime, gcc90, .lags= "2 weeks", .interactive = FALSE)
+df_mid %>% plot_acf_diagnostics(datetime, gcc90, .lags= "1 month", .interactive = FALSE)
+df_mid %>% plot_acf_diagnostics(datetime, GRVI, .lags= "2 weeks", .interactive = FALSE)
+df_mid %>% plot_acf_diagnostics(datetime, GRVI, .lags= "1 month", .interactive = FALSE)
+df_mid %>% plot_acf_diagnostics(datetime, exG, .lags= "2 weeks", .interactive = FALSE)
+df_mid %>% plot_acf_diagnostics(datetime, exG, .lags= "1 month", .interactive = FALSE)
+
+
+
+df_mid %>%  plot_seasonal_diagnostics(datetime, gcc90, .feature_set = c("week", "month.lbl", "quarter"))
+df_mid %>%  plot_seasonal_diagnostics(datetime, GRVI, .feature_set = c("week", "month.lbl", "quarter"))
+df_mid %>%  plot_seasonal_diagnostics(datetime, exG, .feature_set = c("week", "month.lbl", "quarter"))
+
+df_mid %>% plot_anomaly_diagnostics(datetime, gcc90)
+df_mid %>% plot_anomaly_diagnostics(datetime, GRVI)
+df_mid %>% plot_anomaly_diagnostics(datetime, exG)
